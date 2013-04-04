f509b324698e990c1613bdf220fb1952f85eb2ac
Pierre Marchand <pierre@oep-h.com>
2013-04-04 09:47:20 +0200

    Adjusted layout and added functions to message view

--- static/js/views.js
+++ static/js/views.js
@@ -11,6 +11,10 @@ app.MessageView = Backbone.View.extend({
 //         Backbone.View.apply(this, arguments);
 //         this.listenTo(this.model, 'change', this.render);
 //     },
+    selected : false,
+    events:{
+        'click .mail-title': 'toggle',
+    },
     render: function() {
         var data = this.model.toJSON();
         template.render('message', this.$el, function(t){
@@ -18,6 +22,19 @@ app.MessageView = Backbone.View.extend({
         });
         return this;
     },
+    toggle: function(evt){
+        console.log('T');
+        var body = this.$el.find('.message-body');
+        if(this.selected)
+        {
+            body.hide();
+        }
+        else
+        {
+            body.show();
+        }
+        this.selected = !this.selected;
+    }
 });
 
 app.AppView = Backbone.View.extend({
