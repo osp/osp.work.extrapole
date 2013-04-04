b2a467d6818b3be6bb000ed55af6771591165b4d
Pierre Marchand <pierre@oep-h.com>
2013-04-04 12:07:21 +0200

    Finished to render the prototype

--- static/js/app.js
+++ static/js/app.js
@@ -8,6 +8,12 @@ var app = app || {}
 
 $(function() {
     
+    // TODO
+    $('body').append('<div id="about-box"><h1>Présentation</h1><p>Ici pourra venir se loger la présentation d’Extrapole.</p></div>');
+    $('body').append('<div id="agenda-box"><h1>Agenda</h1><p>Et ici pourra venir se loger l’agenda.</p></div>');
+    
+    
     new app.AppView();
+    
     
 });
